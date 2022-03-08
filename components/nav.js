0fc5405f6439de04065e266e51ebe5f85d454798
Tyler Capstick <zzznsewzzz@gmail.com>
2022-03-07 19:15:37 -0600

    updated SPA 2

--- components/nav.js
+++ components/nav.js
@@ -11,15 +11,3 @@ export default links => html`
     </ul>
   </nav>
 `;
-
-// export default () => html`
-//   <nav>
-//     <i class="fas fa-bars"></i>
-//     <ul class="hidden--mobile nav-links">
-//       <li><a href="#index">Home</a></li>
-//       <li><a href="#bio">Bio</a></li>
-//       <li><a href="#pizza">Pizza</a></li>
-//       <li><a href="#order">Order</a></li>
-//     </ul>
-//   </nav>
-// `;
